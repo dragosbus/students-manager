76cc42585a2b731c53a85df209380aaece9230d7
dragosbus <dragosbusuioc94@gmail.com>
2018-04-10 14:10:42 +0200

    studetn are added when the from is submited

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ app = Flask(__name__)
 def student_page():
     if request.method == "POST":
         student_id = request.form.get("student-id", "")
-        student_first_name = request.form.get("student-first-name", "")
-        student_last_name = request.form.get("student-last-name", "")
-        new_student = Student(student_first_name, student_last_name, student_id)
+        first_name = request.form.get("first-name", "")
+        last_name = request.form.get("last-name", "")
+        new_student = Student(first_name, last_name, student_id)
         students.append(new_student)
         
-        return redirect(url_for("students_page"))
+        return redirect(url_for("student_page"))
         
     return render_template("index.html", students=students)
     
